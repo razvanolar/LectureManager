94d66ffa5e1a366629b22fbbcc0d7a81999a411a
razvanolar <rolar.xiss@gmail.com>
2016-10-30 18:19:45 +0200

    Configure mysql connector

---
+++ src/com/google/lecture_manager/server/jdbc/JDBCUtil.java
@@ -0,0 +1,61 @@
+package com.google.lecture_manager.server.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Created by razvanolar on 30.10.2016
+ */
+public class JDBCUtil {
+
+  private static final String HOST = "jdbc:mysql://localhost/mpp";
+  private static final String USER = "root";
+  private static final String PASSWORD = "Changeme.1023";
+
+  public JDBCUtil() {
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void closeStatement(Statement statement) {
+    try {
+      if (statement != null)
+        statement.close();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  private void closeResultSet(ResultSet resultSet) {
+    try {
+      if (resultSet != null)
+        resultSet.close();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void closeConnection(Connection connection) {
+    try {
+      if (connection != null)
+        connection.close();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public Connection getConnection() throws Exception {
+    try {
+      return DriverManager.getConnection(HOST, USER, PASSWORD);
+    } catch (Exception ex) {
+      System.out.println("Repository - getConnection");
+      ex.printStackTrace();
+      throw ex;
+    }
+  }
+}
